38e54e29037d6f095aca54cb89639e5758549294
e-Castro <albertocastrobj@gmail.com>
2019-08-06 17:39:28 -0300

    ALT-06082019

--- consult_usuarios.php
+++ consult_usuarios.php
@@ -45,7 +45,7 @@ include_once 'menu.php';
 
                 //GET com numero da nova pagina
                 /*$pagina = (isset($_GET['agina'])) ? (int)$_GET['pagina'] : 1;*/
-                $pagina = $_GET['pagina']+1;
+                $pagina = isset($_GET['pagina']) ? $_GET['pagina']: 1;
 
                 //numero de intens por paginação
                 $itens_por_pagina = 13;
@@ -64,11 +64,9 @@ include_once 'menu.php';
                 ?>
                
                     <div class="row">
-                        <div class="col-md-1 col-sm-12 col-xs-12 form-group"><label>Ordem</label></div>
-                        <div class="col-md-3 col-sm-12 col-xs-12 form-group"><label>Login</label></div>
-                        <div class="col-md-1 col-sm-12 col-xs-12 form-group"><label>Nível</label></div>
-                        <div class="col-md-2 col-sm-12 col-xs-12 form-group"><label>Código</label></div>
-                        <div class="col-md-5 col-sm-12 col-xs-12 form-group"><label>Obs.</label></div>
+                        <div class="col-md-2 col-sm-12 col-xs-12 form-group"><label>Login</label></div>
+                        <div class="col-md-2 col-sm-12 col-xs-12 form-group"><label>Nível de Acesso</label></div>
+                        <div class="col-md-2 col-sm-12 col-xs-12 form-group"><label>Obs.</label></div>
                     </div>
                     
                 <?php
@@ -80,42 +78,29 @@ include_once 'menu.php';
                         $login = $l["USU_SOC_COD"];
                         $senha = $l['USU_OBS'];
                 ?>
-                    <form action='backend/usu_altera.php?id=<?php echo $id; ?>' method='POST'>
-                        <div class="row">
-                            <div class="col-md-1 col-sm-12 col-xs-12 form-group" style="padding-right: 3px;">
-                                <input type="text" name="id" value="<?php echo $id; ?>" readonly="readonly" class="form-control">
-                            </div>
-                            <div class="col-md-3 col-sm-12 col-xs-12 form-group" style="padding-right: 3px;">
-                                <input type="text" class="form-control" name="nome" value="<?php echo $nome;?>">
-                            </div>
-                            <div class="col-md-1 col-sm-12 col-xs-12 form-group" style="padding-left: 3px; padding-right: 3px;">
-                                <input type="text" class="form-control" name="tipo" value='<?php echo $tipo;?>'>
-                            </div>
-                            <div class="col-md-2 col-sm-12 col-xs-12 form-group" style="padding-left: 3px; padding-right: 3px;">
-                                <select class="form-control select2" name="nivel">
-                                <?php
-                                    if($tipo != 1)
-                                {?>
-                                    <option value="0">Básico</option>
-                                <?php }else{?>
-                                    <option value="1">Administrador</option>
-                                <?php } ?>
-                                    <option value="0">--------------</option>
-                                    <option value="1">Administrador</option>
-                                    <option value="0">Básico</option>
-                                </select>
-                            </div>
-                            <div class="col-xs-5 form-group" style="padding-left: 3px; padding-right: 3px;">
-                                <input type="text" class="form-control" name="login" value='<?php echo $login;?>'>
-                            </div>
-                    
-                            <div class="col-xs-1 form-group" style="padding-left: 3px;">
-                                <button type="submit" class="btn btn-success">Alterar</button>
-                            </div>
-                        </div>
-                    </form>
-                    <?php } ?>
-               
+                    <div class="row">
+                      <div class="col-md-2 col-sm-12 col-xs-12 form-group" style="padding-right: 3px;">
+                        <input type="text" class="form-control" name="nome" value="<?php echo $nome;?>" readonly="readonly">
+                      </div>
+                      <div class="col-md-2 col-sm-12 col-xs-12 form-group" style="padding-left: 3px; padding-right: 3px;">                  
+                      <?php
+                        if($tipo == 0)
+                      {?>
+                        <input type="text" class="form-control" name="nome" value="Básico" readonly="readonly">
+                      <?php }else{?>
+                        <input type="text" class="form-control" name="nome" value="Administrador" readonly="readonly">
+                      <?php } ?>                                  
+                      </div>
+                      <div class="col-xs-4 form-group" style="padding-left: 3px; padding-right: 3px;">
+                        <input type="text" class="form-control" name="login" value='<?php echo $login;?>' readonly="readonly">
+                      </div>
+                      
+                      <div class="col-xs-2 form-group" style="padding-left: 3px;">
+                        <a href="#" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i>Editar</a>
+                        <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete </a>
+                      </div>
+                    </div>  
+                  <?php } ?>
             </div>
           </div>
           <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
